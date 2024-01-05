e4c0663e861ff2d1adb12464aa08d971e55fb4d9
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2024-01-05 03:35:45 -0500

    bble

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  let start = 0;
+  let end = breaks.length;
+  let m = Math.floor((start + end) / 2);
+  let jump = Math.floor(Math.sqrt(breaks.length));
 
+  if (breaks[m] == true) {
+    end = m;
+  } else {
+    start = m + 1;
+  }
+
+  for (let i = start; i < end; i += jump) {
+    if (breaks[i] == true) return i;
+  }
+
+  return -1;
 }
