ebd63c6834ff93335e8dbbd73febed313ae00273
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2024-01-05 17:11:46 -0500

    ques

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>,
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    let new_tail: Node<T> = {
+      value: item,
+      next: undefined
+    };
+
+    if (this.tail) {
+      this.tail.next = new_tail;
     }
 
-    enqueue(item: T): void {
+    if (!this.head) {
+      this.head = new_tail;
+    }
 
-}
-    deque(): T | undefined {
+    this.tail = new_tail;
+    this.length++;
+  }
 
-}
-    peek(): T | undefined {
+  deque(): T | undefined {
+    let dequed_head = this.head;
+    if (!dequed_head) return undefined;
 
-}
+    this.head = dequed_head.next;
+    dequed_head.next = undefined;
+
+    this.length--;
+    return dequed_head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
